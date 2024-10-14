b24bbfaedb07172125a68fe2e1d410833e6dbc4b
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-10-14 18:48:04 +0200

    esercizi arrow functions

---
+++ esercizio-le-arrow-functions/arrow.js
@@ -0,0 +1,4 @@
+//Partendo dall'esercizio precedente, questa volta i numeri da sommare dovranno essere passati come parametro alla funzione
+
+const sum = (a,b) => a + b
+console.log(sum(5, 5));
---
+++ esercizio33-le-arrow-functions/arrow.js
@@ -0,0 +1,11 @@
+//Trasformare la funzione sum in arrow function
+// function sum() {
+//     return 5 + 5;
+//   }
+  
+//   console.log(sum());
+
+const sum = () => { 
+    return 5 + 5 
+}
+console.log(sum());
