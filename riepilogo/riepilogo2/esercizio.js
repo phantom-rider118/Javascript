0e0150992decdcf9a44eb06ef2355de2a86a7f80
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-10-19 15:34:20 +0200

    esercizi riepilogativi

---
+++ riepilogo/riepilogo2/esercizio.js
@@ -0,0 +1,3 @@
+// Scrivi una funzione che prende come parametro un array di oggetti, ognuno dei quali rappresenta una macchina con le seguenti proprietà: marca, modello, anno.
+// La funzione dovrebbe restituire un nuovo array di stringhe che contenga la marca e il modello di ogni macchina nel formato "brand model".
+// Stampa il nuovo array.
