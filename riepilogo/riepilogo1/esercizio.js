4ae8ba65506bc4d8c03384ec254badf7ef979be6
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-10-19 15:33:13 +0200

    traccie esercizi di riepilogo con soluzioni

---
+++ riepilogo/riepilogo1/esercizio.js
@@ -0,0 +1,7 @@
+const students = [
+    { id: 1, name: "Luca", surname: "Rossi", age: 20, hobby: 'gamer'},
+    { id: 2, name: "Mario", surname: "Bianchi", age: 24, hobby: 'sportsman'},
+    { id: 3, name: "Giovanna", surname: "Brambilla", age: 26, hobby: 'gamer'},
+    { id: 4, name: "Carmelo", surname: "Verdi", age: 18, hobby: 'photographer'},
+    ];
+
